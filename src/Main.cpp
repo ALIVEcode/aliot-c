89d6e65bcca44ab3456278020183ab8fbe1b4e51
Merieme Bouisri <meriemebouisri@gmail.com>
2023-03-24 13:22:14 -0400

    Establish websocket connection from class

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,60 +2,42 @@
 #include <WebSocketsClient.h>
 #include <WiFi.h>
 #include <ArduinoJson.h>
+#include "AliotWebSocket.h"
 
 
 // WiFi credentials
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "TransatTelecom19571";
+const char* password = "54a17fd4";
 
-// Websocket credentials
-WebSocketsClient webSocket;
-
-const char* host = "alivecode.ca";
-const char* path = "/iotgateway/";
-const int port = 8881;
+AliotWebSocket AWSClient = AliotWebSocket();
 
 void setupWifi() {
   WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
-void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
-{
-	switch (type)
-	{
-	case WStype_DISCONNECTED:
-		Serial.printf("[WSc] Disconnected!\n");
-		break;
-	case WStype_CONNECTED:
-	{
-		Serial.printf("[WSc] Connected to url: %s\n", payload);
-		webSocket.sendTXT(
-				R"({"event": "connect_object", "data": { "id": "9a38c8ff-45f6-49dd-b38b-0cd1d0fdf981", "token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiRGVmYXVsdCBBdXRoVG9rZW4iLCJvYmplY3RJZCI6IjlhMzhjOGZmLTQ1ZjYtNDlkZC1iMzhiLTBjZDFkMGZkZjk4MSIsImlhdCI6MTY3OTUwODIwMywiZXhwIjo0ODMzMTA4MjAzfQ.wQXHcyV-aHhuQdOLntp4kk1TPORsasa5Gp4DAoXsvQk"}})");
-		
-		break;
-	}
-
-	}
-}
-
 void setup() {
   Serial.begin(115200);
+  pinMode(0, INPUT_PULLUP);
   WiFi.mode(WIFI_STA);
   setupWifi();
-  webSocket.begin(host, port, path);
-  webSocket.onEvent(webSocketEvent);
+  AWSClient.run();
+  AWSClient.event();
 }
 
 void loop() {
-  webSocket.loop();
-
+  if (digitalRead(0) == LOW) {
+    // Disconnect when button is pressed
+    AWSClient.~AliotWebSocket();
+  }
 
 }
