dbf5ae609d14e6e5490d43fdce964b6f4de8765e
Merieme Bouisri <meriemebouisri@gmail.com>
2023-03-25 21:57:49 -0400

    Move most functionalities to Aliot class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,43 +1,34 @@
 #include <Arduino.h>
-#include <WebSocketsClient.h>
-#include <WiFi.h>
-#include <ArduinoJson.h>
-#include "AliotWebSocket.h"
+#include "AliotObject.h"
 
 
 // WiFi credentials
-const char* ssid = "TransatTelecom19571";
-const char* password = "54a17fd4";
+const char* ssid = "";
+const char* password = "";
 
-AliotWebSocket AWSClient = AliotWebSocket();
+// Aliot credentials
+String auth_token = "";
+String object_id = "";
 
-void setupWifi() {
-  WiFi.begin(ssid, password);
+// Create AliotObject instance
+AliotObject AWSClient = AliotObject();
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
 
 void setup() {
   Serial.begin(115200);
   pinMode(0, INPUT_PULLUP);
-  WiFi.mode(WIFI_STA);
-  setupWifi();
+
+  AWSClient.setup_config(auth_token, object_id, ssid, password);
   AWSClient.run();
-  AWSClient.event();
 }
 
 void loop() {
-  if (digitalRead(0) == LOW) {
-    // Disconnect when button is pressed
-    AWSClient.~AliotWebSocket();
-  }
+  AWSClient.m_AWSClient.loop();
 
+  // testing ping
+  if (digitalRead(0) == 0) {
+    // Sending ping will cause server to send pong repeatedly until connection is closed?
+    // TODO: fix that 
+    AWSClient.m_AWSClient.sendTXT("{\"event\":\"ping\", \"data\":null}");
+  }
 }
