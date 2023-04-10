2466964817c43ce76743ee8c230c044e01cb0c4c
Merieme Bouisri <meriemebouisri@gmail.com>
2023-04-10 15:08:48 -0400

    Delete ALIOT_EVENT_MACROS, settle for AliotEvents struct

--- include/ConfigDirectives.h
+++ include/ConfigDirectives.h
@@ -8,14 +8,14 @@
 // Comment out to disable debug output
 #define ALIOT_DEBUG
 
-// Comment out to use the static variables instead of macros
-#define ALIOT_EVENTS_MACROS
+// #define ALIOT_EVENTS_MACROS
 
-#ifdef ALIOT_EVENTS_MACROS
-    #define EVT_CONNECT_SUCCESS "connect_success"
-    #define EVT_CONNECT_OBJECT "connect_object"
-    #define EVT_ERROR "error"
-    #define EVT_PING "ping"
-    #define EVT_PONG "pong"
-    #define EVT_UPDATE_DOC "update_doc"
-#endif /* ALIOT_EVENTS_MACROS */
+// Comment out to use the static variables instead of macros
+// #ifdef ALIOT_EVENTS_MACROS
+//     #define EVT_CONNECT_SUCCESS "connect_success"
+//     #define EVT_CONNECT_OBJECT "connect_object"
+//     #define EVT_ERROR "error"
+//     #define EVT_PING "ping"
+//     #define EVT_PONG "pong"
+//     #define EVT_UPDATE_DOC "update_doc"
+// #endif /* ALIOT_EVENTS_MACROS */
